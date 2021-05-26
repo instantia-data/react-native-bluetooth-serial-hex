30263c9e902116d0e1c4e64881cfaf4dbe88ecbf
luis.pinto <luis.pinto@instantia.pt>
2021-05-26 14:14:18 +0100

    Setup commit

---
+++ android/build/generated/source/buildConfig/debug/com/rusel/RCTBluetoothSerial/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.rusel.RCTBluetoothSerial;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.rusel.RCTBluetoothSerial";
+  public static final String BUILD_TYPE = "debug";
+}
